424f3979edf412426fd2a7a213b87227c8934b95
Luca Somaschini <somas99@gmail.com>
2022-07-25 16:29:29 +0200

    Added new todo in list

--- js/main.js
+++ js/main.js
@@ -1,11 +1,29 @@
 var app = new Vue ({
         el: '#app',
         data: {
-            toDos: [
+            todos: [
                 {
                     text: 'Primo elemento della lista',
                     done: false
+                },
+                {
+                    text: 'secondo elemento della lista',
+                    done: false
+                },
+                {
+                    text: 'terzo elemento della lista',
+                    done: false
                 }
-            ]
+            ],
+            newTodo: {
+                text: '',
+                done: false
+            },
+        },
+        methods: {
+            addNewTodo() {
+                this.todos.push(this.newTodo);
+                console.log(this.todos);
+            }
         }
     })
